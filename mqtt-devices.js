47fa47376415291173d3048e9e9483516bc6f189
Justin Wood <jwood@me.com>
2020-09-03 12:16:38 -0700

    Removing redis_helpers

--- mqtt-devices.js
+++ mqtt-devices.js
@@ -4,26 +4,60 @@ const logging = require('homeautomation-js-lib/logging.js')
 const config = require('homeautomation-js-lib/config_loading.js')
 const _ = require('lodash')
 
-require('homeautomation-js-lib/devices.js')
-require('homeautomation-js-lib/redis_helpers.js')
-
 // Config
 const configPath = process.env.CONFIG_PATH
 
 if (_.isNil(configPath)) {
-	logging.warn('CONFIG_PATH not set, not starting')
-	process.abort()
+    logging.warn('CONFIG_PATH not set, not starting')
+    process.abort()
 }
 
-const redis = Redis.setupClient(function() {
-	config.load_path(configPath)
+const redisHost = process.env.REDIS_HOST
+const redisPort = process.env.REDIS_PORT
+const redisDB = process.env.REDIS_DATABASE
 
-	config.on('config-loaded', () => {
-		logging.info('config-loaded')
-		redis.flushdb()
+const redis = Redis.createClient({
+    host: redisHost,
+    port: redisPort,
+    db: redisDB,
+    retry_strategy: function(options) {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
+            // End reconnecting on a specific error and flush all commands with a individual error
+            return new Error('The server refused the connection')
+        }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+            // End reconnecting after a specific timeout and flush all commands with a individual error
+            return new Error('Retry time exhausted')
+        }
+        if (options.times_connected > 10) {
+            // End reconnecting with built in error
+            return undefined
+        }
+        // reconnect after
+        return Math.min(options.attempt * 100, 3000)
+    }
+})
 
-		config.deviceIterator(function(device_id, device) {
-			redis.set(device.topic, device.name)
-		})
-	})
+// redis callbacks
+
+redis.on('error', function(err) {
+    logging.error('redis error ' + err)
+})
+
+redis.on('connect', function() {
+    logging.info('redis connected')
+    config.load_path(configPath)
+
+    config.on('config-loaded', () => {
+        logging.info('config-loaded')
+        redis.flushdb()
+
+        config.deviceIterator(function(device_id, device) {
+            redis.set(device.topic, device.name)
+        })
+    })
+})
+
+redis.on('reconnect', function() {
+    logging.info('redis disconnected')
 })
